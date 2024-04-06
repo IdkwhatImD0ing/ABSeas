08b7ff7bb12f3d6c35f1667c474d50c72e8eb1e2
Dylan Vu <dylanvu9@gmail.com>
2024-04-06 14:28:19 -0700

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI, Response, HTTPException, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/song")
+def get_song():
