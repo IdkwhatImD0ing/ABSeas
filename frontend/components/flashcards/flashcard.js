e3fd1feabf38afb735435f1d86d7abbeecea0511
Jasmine <jasmineqw45@gmail.com>
2024-04-07 02:21:31 -0700

    added flashcards to page

--- frontend/components/flashcards/flashcard.js
+++ frontend/components/flashcards/flashcard.js
@@ -1,21 +1,32 @@
-import {Box} from '@mui/material'
-import Flip from "react-card-flip"
+import { Box, Typography } from "@mui/material";
+import Flip from "react-card-flip";
 // learnName = what is being learned ex: 1, 2, 3, in counting
 // cardName = associated imagery demonstrated by the card img
-function flashcards({cardImg, cardName, learnName}) {
+function flashcards({ cardImg, cardName, learnName }) {
   return (
-   <Box
-    sx={{
-        padding:'36px'
-
-    }}
-   >
-        <img src={cardImg} />
-        <h2>
-            {cardName}
-        </h2>
-   </Box>
-  )
+    <Box
+      sx={{
+        padding: "36px",
+        border: "6px solid #D1E4EE",
+        borderRadius: "22px",
+        width: "fit-content",
+      }}
+    >
+      <img src={cardImg} />
+      <Typography
+        sx={{
+          fontSize: "4rem",
+          color: "#535562",
+          fontWeight: "900",
+        }}
+      >
+        {cardName}
+      </Typography>
+      <Typography sx={{}}>
+        {learnName}
+      </Typography>
+    </Box>
+  );
 }
 
-export default flashcards
+export default flashcards;
