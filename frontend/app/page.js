969f9a43ab0ba01c737d9feb2897f42b23f8102f
Bill Zhang <billzhangsc@gmail.com>
2024-04-07 00:43:10 -0700

    Home basics

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <></>
+import Home from '@/components/home/home'
+import {Box} from '@mui/material'
+export default function Default() {
+  return (
+    <Box width="100vw" height="100vh" top={0} left={0} overflow={'hidden'}>
+      <Home />
+    </Box>
+  )
 }
