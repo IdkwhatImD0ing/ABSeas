addf4da21bd9f7b9c559fba403a5f8af080ae989
Bill Zhang <billzhangsc@gmail.com>
2024-04-07 02:34:06 -0700

    Does work finally

--- frontend/components/home/home.js
+++ frontend/components/home/home.js
@@ -14,6 +14,7 @@ export default function Home() {
       display="flex"
       alignItems="center"
       justifyContent="center"
+      padding="36px"
       sx={{
         backgroundImage: 'url(/home.svg)',
         backgroundSize: 'cover',
