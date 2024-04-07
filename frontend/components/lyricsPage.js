7f9c61b583b4687ab9e3703203cd521f0029cb28
Bill Zhang <billzhangsc@gmail.com>
2024-04-07 07:21:05 -0700

    IT WORKS

--- frontend/components/lyricsPage.js
+++ frontend/components/lyricsPage.js
@@ -1,30 +1,50 @@
-import React from 'react'
+import React, {useContext, useEffect, useRef} from 'react'
+import SongContext from './SongContext'
+import {Box, Typography} from '@mui/material'
 
-function lyricsPage({lyrics}) {
+function LyricsPage() {
+  const {song} = useContext(SongContext)
+  const audioElementRef = useRef(null)
+
+  useEffect(() => {
+    if (song.mediaSource && audioElementRef.current) {
+      const objectURL = URL.createObjectURL(song.mediaSource)
+      audioElementRef.current.src = objectURL
+      audioElementRef.current.play().catch((error) => {
+        console.error('Autoplay failed', error)
+      })
+
+      return () => {
+        URL.revokeObjectURL(objectURL) // Clean up when the component unmounts or the source changes
+      }
+    }
+  }, [song.mediaSource])
+
   return (
     <Box
-        display="flex"
-        justifyContent="flex-start"
-        width= "fit-content"
-        padding= "36px"
-        border= "6px solid #D1E4EE"
-        borderRadius= "22px"
-    > 
+      display="flex"
+      justifyContent="flex-start"
+      width="fit-content"
+      padding="36px"
+      border="6px solid #D1E4EE"
+      borderRadius="22px"
+    >
+      {song.lyrics && (
         <Typography
-            sx={{
-                color: "#535562",
-                textAlign: "center",
-                fontFamily: "Poppins",
-                fontSize: "2.75625rem",
-                fontWeight: "800",
-              }}
+          sx={{
+            color: '#535562',
+            textAlign: 'center',
+            fontFamily: 'Poppins',
+            fontSize: '2.75625rem',
+            fontWeight: '800',
+          }}
         >
-            {lyrics} 
+          {song.lyrics}
         </Typography>
-       
+      )}
+      <audio ref={audioElementRef} controls />
     </Box>
-    
   )
 }
 
-export default lyricsPage
+export default LyricsPage
